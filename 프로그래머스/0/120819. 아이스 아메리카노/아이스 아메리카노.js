994fb7b760694b16c18321d3927da0871e9aa265
Just0neMoment <dnxks1222@gmail.com>
2024-04-25 11:24:16 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -0,0 +1,7 @@
+function solution(money) {
+  const pricePerCup = 5500;
+  const maxCups = Math.floor(money / pricePerCup);
+  const remainingMoney = money % pricePerCup;
+
+  return [maxCups, remainingMoney];
+}
