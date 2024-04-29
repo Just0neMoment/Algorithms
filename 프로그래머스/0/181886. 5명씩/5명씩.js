4b1a76cb089877451db79feb9362967a5e6bb14f
Just0neMoment <dnxks1222@gmail.com>
2024-04-29 09:49:12 +0900

    [level 0] Title: 5명씩, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181886. 5명씩/5명씩.js
@@ -0,0 +1,9 @@
+function solution(names) {
+    let result = [];
+    for(i=0; i<names.length; i++){
+        if((i+5) % 5 === 0){
+            result.push(names[i])
+        }
+    }
+    return result;
+}
