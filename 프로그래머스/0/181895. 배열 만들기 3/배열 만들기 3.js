ab16db702c096d08cf8841a3747a115fc189c4f1
홍연우 <dnxks1222@gmail.com>
2024-05-04 11:49:20 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.09 ms, Memory: 37.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.js
@@ -0,0 +1,9 @@
+function solution(arr, intervals) {
+    let firstArr = arr.slice(intervals[0][0], intervals[0][1]+1)
+    let secondArr = arr.slice(intervals[1][0], intervals[1][1]+1)
+    let result = [];
+    
+    result.push(...firstArr)
+    result.push(...secondArr)
+    return result
+}
